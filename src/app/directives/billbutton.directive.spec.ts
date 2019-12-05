1c0978c8f32d3fad5fbeba0e3676c3602591e10c
tychang <tychang@Nexify.com.hk>
2019-12-05 12:01:51 +0800

    .

--- src/app/directives/billbutton.directive.spec.ts
+++
@@ -1,8 +0,0 @@
-import { BillbuttonDirective } from './billbutton.directive';
-
-describe('BillbuttonDirective', () => {
-  it('should create an instance', () => {
-    const directive = new BillbuttonDirective();
-    expect(directive).toBeTruthy();
-  });
-});
--- src/app/directives/billbutton.directive.ts
+++
@@ -1,8 +0,0 @@
-import { Directive, ElementRef } from '@angular/core';
-
-@Directive({
-  selector: '[appBillbutton]'
-})
-export class BillbuttonDirective {
-  constructor(private el: ElementRef) {}
-}
